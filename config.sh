9e882b657b865c8e35a33884969507619f56603a
cnjax <nightcat@gmail.com>
2017-01-16 10:03:50 +0800

    fix the line break

--- config.sh
+++ config.sh
@@ -1,9 +1,1 @@
-./configure --prefix=/usr/local/php56 --with-pdo-pgsql --with-zlib-dir --with-freetype-dir --enable-m
-bstring --with-libxml-dir=/usr --enable-soap --enable-calendar --with-curl --with-mcrypt --with-zlib 
---with-gd=/usr --with-pgsql --disable-rpath --enable-inline-optimization --with-bz2 --with-zlib --ena
-ble-sockets --enable-sysvsem --enable-sysvshm --enable-pcntl --enable-mbregex --enable-exif --enable-
-bcmath --with-mhash --enable-zip --with-pcre-regex --with-mysql=/usr --with-pdo-mysql --with-mysqli=/
-usr/bin/mysql_config --with-jpeg-dir=/usr --with-png-dir=/usr --enable-gd-native-ttf --with-openssl -
--with-fpm-user=www --with-fpm-group=www --with-libdir=/lib/x86_64-linux-gnu --enable-ftp  --with-kerb
-eros --with-gettext --with-xmlrpc --with-xsl=/usr --enable-opcache --enable-fpm --libdir=/usr/lib64 -
--with-libdir=lib64 --with-iconv-dir=/usr 
+./configure --prefix=/usr/local/php56 --with-pdo-pgsql --with-zlib-dir --with-freetype-dir --enable-mbstring --with-libxml-dir=/usr --enable-soap --enable-calendar --with-curl --with-mcrypt --with-zlib --with-gd=/usr --with-pgsql --disable-rpath --enable-inline-optimization --with-bz2 --with-zlib --enable-sockets --enable-sysvsem --enable-sysvshm --enable-pcntl --enable-mbregex --enable-exif --enable-bcmath --with-mhash --enable-zip --with-pcre-regex --with-mysql=/usr --with-pdo-mysql --with-mysqli=/usr/bin/mysql_config --with-jpeg-dir=/usr --with-png-dir=/usr --enable-gd-native-ttf --with-openssl --with-fpm-user=www --with-fpm-group=www --with-libdir=/lib/x86_64-linux-gnu --enable-ftp  --with-kerberos --with-gettext --with-xmlrpc --with-xsl=/usr --enable-opcache --enable-fpm --libdir=/usr/lib64 --with-libdir=lib64 --with-iconv-dir=/usr  --sysconfdir=/usr/local/php56/etc
